573189aaf7ac75bcbcc1ca7430e2f3a69b5f1ecb
Juraj <info@antwise.sk>
2026-02-28 11:06:32 +0100

    initial app files

---
+++ GeoHero/GeoHero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoHero
+//
+//  Created by AntWise s.r.o. on 28/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeoHero/GeoHero/GeoHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoHeroApp.swift
+//  GeoHero
+//
+//  Created by AntWise s.r.o. on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GeoHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
